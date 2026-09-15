022be021ded4628b766c3c47db7eec17b288e5a0
KeshanDevil <87665135+KeshanDevil@users.noreply.github.com>
2026-09-15 22:21:44 +0530

    Add files via upload

---
+++ Q1/Lab2Q1.java
@@ -0,0 +1,17 @@
+//////////IT 21047756\\\\\\
+public class Lab2Q1 {
+    public static void main(String[] args) {
+
+        // Given perimeter
+        double perimeter = 100;
+
+        // Width is 3/4 of the length
+        // width = (3/4) * length
+        double length = perimeter / 3.5;
+        double width = (3.0 / 4.0) * length;
+
+        // Display results
+        System.out.println("Length of the fence = " + length);
+        System.out.println("Width of the fence = " + width);
+    }
+}
