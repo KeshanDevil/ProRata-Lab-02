a36b3d4735e53a66c323b3f4fc5b033e0598db14
KeshanDevil <87665135+KeshanDevil@users.noreply.github.com>
2026-09-15 22:22:29 +0530

    Add files via upload

---
+++ Q3/Lab2Q3.java
@@ -0,0 +1,15 @@
+/////IT21047756\\\\\\
+public class Lab2Q3 {
+    public static void main(String[] args) {
+
+        // Given sides
+        double sideA = 3;
+        double sideB = 4;
+
+        // Calculate hypotenuse
+        double hypotenuse = Math.sqrt((sideA * sideA) + (sideB * sideB));
+
+        // Display result
+        System.out.println("Length of the hypotenuse = " + hypotenuse);
+    }
+}
